de06f3ceb302b9c79615c04b4b8b5b00493a8354
Rasmus <rasmus@christensen.io>
2018-12-03 12:42:18 +0100

    Merged, and kept the correct code. I think

--- Link/Link.cs
+++ Link/Link.cs
@@ -33,16 +33,15 @@ namespace Linklaget
 
         public void Send(byte[] buf, int size)
         {
-<<<<<<< HEAD
 			// TO DO Your own code
 
 			int index= 0;
 			buffer[index] = DELIMITER;
 			++index;
-=======
-            List<Byte> byteList = new List<Byte>();
-            byteList.Add(DELIMITER);
->>>>>>> 96c34c9e9a1fa7e9ec8ae77634ea05a1c6a03f93
+
+            //Annes kode fra merge
+            //List<Byte> byteList = new List<Byte>();
+            //byteList.Add(DELIMITER);
 
             for (int i = 0; i < size; i++)
             {
@@ -65,74 +64,61 @@ namespace Linklaget
 					buffer[index] = buf[i];
 					++index;
                 }
-            }
-<<<<<<< HEAD
-
+            }         
 			buffer[index] = (byte)'A';
 			++index;
             
             serialPort.Write(buffer, 0, index);         
-=======
-            byteList.Add(DELIMITER);
-            buffer = byteList.OfType<byte>().ToArray();
-            serialPort.Write(buffer, 0, buffer.Length);
->>>>>>> 96c34c9e9a1fa7e9ec8ae77634ea05a1c6a03f93
+
+            //Annes kode fra merge
+            //byteList.Add(DELIMITER);
+            //buffer = byteList.OfType<byte>().ToArray();
+            //7serialPort.Write(buffer, 0, buffer.Length);
+
         }
-      
-        public int Receive(ref byte[] buf)
-        {
-            List<byte> byteList = new List<byte>();
-            List<byte> finalByteList = new List<byte>();
-            int readbyte = 0;
+
+		public int Receive(ref byte[] buf)
+		{
+			List<byte> byteList = new List<byte>();
+			List<byte> finalByteList = new List<byte>();
+			int readbyte = 0;
 			var checkOnA = (int)Convert.ToByte('A');
 
 			while (readbyte != checkOnA)
-            {
-                readbyte = serialPort.ReadByte();
-            }
-            readbyte = 0;
+			{
+				readbyte = serialPort.ReadByte();
+			}
+			readbyte = 0;
 
 			while (readbyte != checkOnA)
-            {
-                readbyte = serialPort.ReadByte();
-<<<<<<< HEAD
-=======
-                byteList.Add((byte)readbyte);				
-            }
->>>>>>> 96c34c9e9a1fa7e9ec8ae77634ea05a1c6a03f93
-
+			{
+				readbyte = serialPort.ReadByte();
+                
 				if (readbyte == (byte)'B')
-                    {
-    					if(readbyte == (byte)'C')
-    						finalByteList.Add((byte)'A');
-                    }
-				else if (readbyte == (byte)'B')
-                    {
-    					if(readbyte == (byte)'D')
-                            finalByteList.Add((byte)'B');
-                    }
-				else if(readbyte == (byte)'A')
-    				{
-    				    Console.WriteLine("End of array...");
+				{
+					if (readbyte == (byte)'C')
+						finalByteList.Add((byte)'A');
 				}
-                else
-<<<<<<< HEAD
-                    {
-					    finalByteList.Add((byte)readbyte);
-                    }            
+				else if (readbyte == (byte)'B')
+				{
+					if (readbyte == (byte)'D')
+						finalByteList.Add((byte)'B');
+				}
+				else if (readbyte == (byte)'A')
+				{
+					Console.WriteLine("End of array...");
+				}
+				else
+				{
+					finalByteList.Add((byte)readbyte);
+				}
 			}
 
             buf = finalByteList.OfType<byte>().ToArray();
-            
-=======
-                {
-					finalByteList.Add(byteList[i]);
-                }
-            }
-            buf = finalByteList.OfType<byte>().ToArray();
->>>>>>> 96c34c9e9a1fa7e9ec8ae77634ea05a1c6a03f93
+
             return buf.Length;
         }
+
         public byte[] GetBuffer()
 		{
 			return buffer; 
